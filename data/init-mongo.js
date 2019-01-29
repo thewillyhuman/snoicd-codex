cc3b0b4ec6d35b5d52fbc9b381b87d8b1cdd0142
Guillermo Facundo Colunga <colunga91@gmail.com>
2019-01-29 17:51:34 +0100

    [apiV2] added configuration for api v2

--- data/init-mongo.js
+++ data/init-mongo.js
@@ -1,8 +1,20 @@
 let res = [
-  db.createCollection("internalKB"),
-  db.internalKB.drop(),
-  db.internalKB.drop(),
-  db.internalKB.createIndex({ snomedCode: 1 }, { name: "snomedCode"}),
-  db.internalKB.createIndex({ icd9Code: 1 }, { name: "icd9Code"}),
-  db.internalKB.createIndex({ icd10Code: 1 }, { name: "icd10Code"}),
+    db.createCollection("concepts"),
+    db.concepts.drop(),
+    db.concepts.drop(),
+    db.concepts.createIndex({
+        code: 1
+    }, {
+        name: "code"
+    }),
+    db.concepts.createIndex({
+        descriptions: 1
+    }, {
+        name: "descriptions"
+    }),
+    db.concepts.createIndex({
+        description: 1
+    }, {
+        name: "description"
+    }),
 ]
