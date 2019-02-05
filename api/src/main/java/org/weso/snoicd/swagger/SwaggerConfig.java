f63fcdedd688114f3a8c9b276aea3f78114ef9cf
Guillermo Facundo Colunga <colunga91@gmail.com>
2019-02-05 11:53:31 +0100

    [swagger] fixed swagger configuration
    Fixes the swagger configuration

--- api/src/main/java/org/weso/snoicd/swagger/SwaggerConfig.java
+++ api/src/main/java/org/weso/snoicd/swagger/SwaggerConfig.java
@@ -12,13 +12,14 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-
+	
 	@Bean
-    public Docket api() { 
-        return new Docket(DocumentationType.SWAGGER_2)  
-          .select()                                  
-          .apis(RequestHandlerSelectors.any())              
-          .paths(PathSelectors.any())                          
-          .build();                                           
+    public Docket apiDocket() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/api/*"))
+                .build();
     }
+
 }
