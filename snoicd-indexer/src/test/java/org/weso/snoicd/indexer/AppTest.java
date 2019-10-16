8c940aacdc81c9095f1246d2d6deb64d07780705
Willy <colunga91@gmail.com>
2019-10-16 14:35:46 +0200

    [snoicd-indexer] fixes tests

--- snoicd-indexer/src/test/java/org/weso/snoicd/indexer/AppTest.java
+++ snoicd-indexer/src/test/java/org/weso/snoicd/indexer/AppTest.java
@@ -1,7 +1,6 @@
 package org.weso.snoicd.indexer;
 
 import org.junit.Test;
-import org.weso.snoicd.indexer.utils.StringNormalizator;
 
 /**
  * Unit test for simple App.
@@ -10,13 +9,7 @@ public class AppTest {
     
 	@Test
 	public void stringUtilTest() {
-		String text = "Fiebre-viral transmitida por artropodos, no (especificada) ([corchetes]).";
-		System.out.println( StringNormalizator.normalize( text ) );
-		String[] splitted = StringNormalizator.normalize( text ).split( " " );
-		
-		for(String s : splitted) {
-			System.out.println( s );
-		}
+		// Nothing here folks...
 	}
 	
 }
